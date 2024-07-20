4347487055a13c8b88ae6e2f294fcb2ff1806d89
Hanhan <764932129@qq.com>
2024-07-20 15:38:34 +0800

    feat: make new factory of Variable

---
+++ src/main/java/me/xiaoying/serverbuild/factory/VariableFactory.java
@@ -0,0 +1,48 @@
+package me.xiaoying.serverbuild.factory;
+
+import me.xiaoying.serverbuild.utils.DateUtil;
+import org.bukkit.entity.Player;
+
+/**
+ * Factory Variable
+ */
+public class VariableFactory {
+    private String string;
+
+    public VariableFactory(String string) {
+        this.string = string;
+    }
+
+    public VariableFactory date(String format) {
+        this.string = this.string.replace("%date%", DateUtil.getDate(format));
+        return this;
+    }
+
+    public VariableFactory player(Player player) {
+        return this.player(player.getDisplayName());
+    }
+
+    public VariableFactory player(String player) {
+        this.string = this.string.replace("%player%", player);
+        return this;
+    }
+
+    public VariableFactory prefix(String prefix) {
+        this.string = this.string.replace("%prefix%", prefix);
+        return this;
+    }
+
+    public VariableFactory time(int time) {
+        return this.time(String.valueOf(time));
+    }
+
+    public VariableFactory time(String time) {
+        this.string = this.string.replace("%time%", time);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return this.string;
+    }
+}
