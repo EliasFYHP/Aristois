046c8f8666572bd5695d7f65b7c3f5e0623ae745
Hanhan <764932129@qq.com>
2024-07-23 00:18:26 +0800

    feat: Constant of ResolveLag.yml

---
+++ src/main/java/me/xiaoying/serverbuild/constant/ResolveLagConstant.java
@@ -0,0 +1,34 @@
+package me.xiaoying.serverbuild.constant;
+
+import java.util.List;
+
+/**
+ * Constant of file for ResolveLag
+ */
+public class ResolveLagConstant {
+    public static boolean ENABLE;
+
+    public static String SETTING_DATEFORMAT, SETTING_PREFIX;
+
+    public static String RESOLVE_LAG_SECOND_TIME;
+    public static boolean RESOLVE_LAG_ENTITY_PET,
+            RESOLVE_LAG_ENTITY_NAMED,
+            RESOLVE_LAG_ENTITY_SPECIAL_POSE,
+            RESOLVE_LAG_ENTITY_TOTAL_ENABLE;
+
+    public static long RESOLVE_LAG_ENTITY_TOTAL_LIMIT;
+    public static List<String> RESOLVE_LAG_ENTITY_SPECIAL_ITEM,
+            RESOLVE_LAG_ENTITY_SPECIAL_ENTITY;
+
+    public static long RESOLVE_LAG_CHUNK_INTERVAL, RESOLVE_LAG_TOTAL_LIMIT;
+    public static boolean RESOLVE_LAG_TOTAL_ENABLE;
+
+    public static List<String> RESOLVE_LAG_WHITE_WORLD;
+    public static List<Long> RESOLVE_LAG_COUNT_TIME;
+
+    public static String CLEAR_MESSAGE_COUNT_DOWN;
+
+    public static String MESSAGE_RELOAD,
+            MESSAGE_MISSING_PERMISSION,
+            MESSAGE_HELP;
+}
